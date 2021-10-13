8d51e97d6dd742b8f9f6839323c6e25ab580c57c
serge-thui <serge74.thuillier@gmail.com>
2021-10-13 13:15:47 +0200

    declare variables ok

--- 01-variables/2-declare-variables/script.js
+++ 01-variables/2-declare-variables/script.js
@@ -1,4 +1,4 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+
+let ourPlanet="Earth";
+let visitorName="Tarzan";
+
