cb0516ef2288614bfd25653adbe1310a1b375709
serge-thui <serge74.thuillier@gmail.com>
2021-10-13 14:02:54 +0200

    if zero string ok

--- 06-ifelse/1-if-zero-string/script.js
+++ 06-ifelse/1-if-zero-string/script.js
@@ -1,4 +1,5 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+if ("0") {
+      alert( 'Hello' );
+    }
+
+// "0" est true donc on affiche bien hello
