eaacc9225c3cf0d2d0556ce87d2446492ff47d68
serge-thui <serge74.thuillier@gmail.com>
2021-10-13 14:18:43 +0200

    check standard ok

--- 06-ifelse/2-check-standard/script.js
+++ 06-ifelse/2-check-standard/script.js
@@ -1,4 +1,2 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let result = prompt("Quel est le nom de Javascript?", "");
+(result=="ECMAScript") ? alert("Bonne réponse") : alert("Vous ne connaissez pas? ECMAScript!");
