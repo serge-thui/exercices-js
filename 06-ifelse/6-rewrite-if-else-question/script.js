eb9bbbad6849f0da207f998f67d5a7381dd26639
serge-thui <serge74.thuillier@gmail.com>
2021-10-13 14:35:21 +0200

    rewrite if else

--- 06-ifelse/6-rewrite-if-else-question/script.js
+++ 06-ifelse/6-rewrite-if-else-question/script.js
@@ -1,4 +1,22 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
+/*
+Réécrire ce if..else en utilisant plusieurs opérateurs ternaires '?'.
+
+Pour plus de lisibilité, il est recommandé de diviser le code en plusieurs lignes.
+
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
 */
+
+let message = (login=='Employee') ? 'Hello' : 
+            (login=='Director') ? 'Greetings' :
+            (login=='')? 'No login':
+            '';
