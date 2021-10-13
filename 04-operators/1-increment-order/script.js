dc4eb8edfb8e3d45358171cfac458f27801eeb7b
serge-thui <serge74.thuillier@gmail.com>
2021-10-13 13:38:03 +0200

    operators ok

--- 04-operators/1-increment-order/script.js
+++ 04-operators/1-increment-order/script.js
@@ -1,4 +1,5 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let a = 1, b = 1;
+
+let c = ++a; // 
+let d = b++; //
+// a=2 b=2 c=2 d=1
