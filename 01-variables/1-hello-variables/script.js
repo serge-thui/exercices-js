e87945154d458bab0b8ba021cec0199bc1d0782a
serge-thui <serge74.thuillier@gmail.com>
2021-10-13 12:58:44 +0200

    variables ok

--- 01-variables/1-hello-variables/script.js
+++ 01-variables/1-hello-variables/script.js
@@ -1,4 +1,14 @@
+// exo 1
 let admin, name;
 name="John";
 admin=name;
 alert(admin);
+
+// exo 2
+let ourPlanet="Earth";
+let visitorName="Tarzan";
+
+//exo 3
+pour la const birthday oui il faut mettre en majuscule
+pour la const age je mettrais en majuscule comme on emploie const 
+mais cela peut-être facultatif comme elle est assignée à un calcul pas forcément Constant.
