0c7d408d5a854ec04a4fc7741f7e665efc68f34a
serge-thui <serge74.thuillier@gmail.com>
2021-10-13 14:28:58 +0200

    5-rewrite if

--- 06-ifelse/5-rewrite-if-question/script.js
+++ 06-ifelse/5-rewrite-if-question/script.js
@@ -1,4 +1,11 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+/*Réécrivez ce if en utilisant l'opérateur conditionnel '?' :
+
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+} */
+
+let result = (a+b<4) ? 'Below' : 'Over';
