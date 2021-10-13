096fc9ff3f47da055a293fc60de9b4a3b84a33dc
serge-thui <serge74.thuillier@gmail.com>
2021-10-13 13:31:28 +0200

    exo ok

--- 03-alert-prompt-confirm/1-simple-page/script.js
+++ 03-alert-prompt-confirm/1-simple-page/script.js
@@ -1,4 +1,2 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let yourName=prompt("Quel est votre nom?", "");
+alert(yourName);
