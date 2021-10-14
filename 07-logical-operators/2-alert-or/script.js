f1c300ad6a9b83849aed249ca575432cc5c1b873
serge-thui <serge74.thuillier@gmail.com>
2021-10-14 11:43:02 +0200

    2-alert-or ok

--- 07-logical-operators/2-alert-or/script.js
+++ 07-logical-operators/2-alert-or/script.js
@@ -1,4 +1,4 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+alert( alert(1) || 2 || alert(3) );// alert(1) retourne undefined (false dans le or) 
+                                   //mais l'affichage s'effectue et on passe au prochain qui est la valeur 2
+                                   //on affiche alors 2 et on sort de l'alert car elle pass à true
+                                   //on affiche donc une première alert avec 1 puis une seconde avec 2
