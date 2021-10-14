b6d142137afac9e0e009eea2892ab147de22fc48
serge-thui <serge74.thuillier@gmail.com>
2021-10-14 13:37:11 +0200

    rewrite switch ok

--- 09-switch/1-rewrite-switch-if-else/script.js
+++ 09-switch/1-rewrite-switch-if-else/script.js
@@ -1,4 +1,25 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+switch (browser) {
+      case 'Edge':
+        alert( "You've got the Edge!" );
+        break;
+    
+      case 'Chrome':
+      case 'Firefox':
+      case 'Safari':
+      case 'Opera':
+        alert( 'Okay we support these browsers too' );
+        break;
+    
+      default:
+        alert( 'We hope that this page looks ok!' );
+    }
+
+ //mettre en 'if'
+ 
+ if (browser=='Edge'){
+       alert( "You've got the Edge!");
+ }else if ( browser=='Chrome' || browser=='Firefox' || browser=='Safari' || browser=='Opera'){
+      alert( 'Okay we support these browsers too' );
+ }else {
+ alert( 'We hope that this page looks ok!' );
+ }
