1d388be4f4ef18da96f8338bf170fe82589c56e7
serge-thui <serge74.thuillier@gmail.com>
2021-10-14 12:09:23 +0200

    check if in range og

--- 07-logical-operators/6-check-if-in-range/script.js
+++ 07-logical-operators/6-check-if-in-range/script.js
@@ -1,4 +1,8 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
+/*  Ecrivez une condition "if" pour vérifier que l’age est compris entre 14 et 90 ans inclus.
+
+"Inclus" signifie que l’age peut atteindre les 14 ou 90 ans.
 */
+(age<14||age>90) ? 
+// ou
+
+if (age<14||age>90){}
