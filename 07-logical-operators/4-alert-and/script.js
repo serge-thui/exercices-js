f96e21538230e6fa3f5def2b768dcf427f7e60f5
serge-thui <serge74.thuillier@gmail.com>
2021-10-14 11:52:55 +0200

    alert and ok

--- 07-logical-operators/4-alert-and/script.js
+++ 07-logical-operators/4-alert-and/script.js
@@ -1,4 +1,3 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+alert( alert(1) && alert(2) );// tout comme pour le or on a alert(1) qui retourne undefined mais s'effectue dans un premier temps
+                              // comme alert(1)est undefined alors l'alert principal passe a undefined donc false
+                              // on affiche l'alert(1) puis on affiche undefined et on ne tient pas compte de l'alert(2)
