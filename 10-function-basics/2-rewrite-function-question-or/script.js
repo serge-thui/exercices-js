ef78ee12f32d5119e2befb589fb1485205ced6a4
serge-thui <serge74.thuillier@gmail.com>
2021-10-14 13:58:57 +0200

    rewrite function ok

--- 10-function-basics/2-rewrite-function-question-or/script.js
+++ 10-function-basics/2-rewrite-function-question-or/script.js
@@ -1,4 +1,19 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+function checkAge(age) {
+      if (age > 18) {
+        return true;
+      } else {
+        return confirm('Did parents allow you?');
+      }
+    }
+ 
+    // variante avec ?
+
+    function checkAge(age){
+      return (age>18) ? true : confirm('Did parents allow you?')
+    }
+
+    // variante avec ||
+
+    function checkAge(age){
+      return (age>18) || confirm('Did parents allow you?')   
+    }
