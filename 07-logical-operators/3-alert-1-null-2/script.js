48db8c612d4d09bf5d71ccf6cce32881b3c559a6
serge-thui <serge74.thuillier@gmail.com>
2021-10-14 11:46:46 +0200

    alert-1-null ok

--- 07-logical-operators/3-alert-1-null-2/script.js
+++ 07-logical-operators/3-alert-1-null-2/script.js
@@ -1,4 +1,1 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+alert( 1 && null && 2 );// alert affiche null comme null est false 
