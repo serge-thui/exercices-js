a89d798866594650b89e3b704c8f0e68f476287d
serge-thui <serge74.thuillier@gmail.com>
2021-10-14 12:03:15 +0200

    alert-and-or ok

--- 07-logical-operators/5-alert-and-or/script.js
+++ 07-logical-operators/5-alert-and-or/script.js
@@ -1,4 +1,8 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+alert( null || 2 && 3 || 4 );// la priorité des opérateurs && est supérieure au OR
+                             // on fait 2 && 3 en premier
+                             // && renvoie soit la première valeur fausse ou si tout est true la dernière valeur
+                             // 2 && 3 donne 3
+                             // on se retrouve avec alert( null || 3 || 4)
+                             // la première valeur true est 3 (car nous sommes sur des OR)
+                             // on affiche alors 3
+
