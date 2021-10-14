52f921a9c74a0126b40e3ea92cebb40612c2334e
serge-thui <serge74.thuillier@gmail.com>
2021-10-14 14:28:11 +0200

    arrow function basics ok

--- 11-arrow-functions-basics/1-rewrite-arrow/script.js
+++ 11-arrow-functions-basics/1-rewrite-arrow/script.js
@@ -1,4 +1,23 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+function ask(question, yes, no) {
+      if (confirm(question)) yes();
+      else no();
+    }
+    
+    ask(
+      "Do you agree?",
+      function() { alert("You agreed."); },
+      function() { alert("You canceled the execution."); }
+    );
+
+    // Remplacez les expressions de fonction par des fonctions fléchées dans le code ci-dessous :
+
+    function ask(question, yes, no) {
+      if (confirm(question)) yes();
+      else no();
+    }
+    
+    ask(
+      "Do you agree?",
+      () =>  alert("You agreed."),
+      () =>  alert("You canceled the execution.")
+    ); 
