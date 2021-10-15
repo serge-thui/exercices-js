0abf3dfa19d42662be89a72e547a624bf5a33ca5
serge-thui <serge74.thuillier@gmail.com>
2021-10-15 09:51:41 +0200

    replace for while

--- 12-while-for/5-replace-for-while/script.js
+++ 12-while-for/5-replace-for-while/script.js
@@ -1,4 +1,11 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+for (let i = 0; i < 3; i++) {
+      alert( `number ${i}!` );
+    }
+ //  éécrivez le code en modifiant la boucle for en while sans modifier son comportement
+ 
+ let i=0;
+
+ while ( i <3 ){
+      alert( `number ${i}!` );
+      i++
+ }
