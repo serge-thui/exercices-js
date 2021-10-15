f81599cebef1e46e7cdb2ba384436a9e08c6d44f
serge-thui <serge74.thuillier@gmail.com>
2021-10-15 09:59:05 +0200

    repeat until correct

--- 12-while-for/6-repeat-until-correct/script.js
+++ 12-while-for/6-repeat-until-correct/script.js
@@ -1,4 +1,11 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+// Ecrivez une boucle qui demande un nombre supérieur à 100. Si le visiteur saisit un autre numéro, demandez-lui de le saisir à nouveau.
+
+// La boucle doit demander un numéro jusqu'à ce que le visiteur saisisse un nombre supérieur à 100 ou annule l'entrée/entre une ligne vide.
+
+// Ici, nous pouvons supposer que le visiteur ne saisit que des chiffres. Il n’est pas nécessaire de mettre en œuvre un traitement spécial pour une entrée non numérique dans cette tâche.
+
+let rep=0;
+
+while ( rep<=100 ){
+      rep = +prompt("Veuillez saisir un nombre supérieur à 100 :");
+}
