9e7dd9036f25d02a8eda5cea8fe756bfb379eabe
serge-thui <serge74.thuillier@gmail.com>
2021-10-15 09:28:34 +0200

    which-value-while ok

--- 12-while-for/2-which-value-while/script.js
+++ 12-while-for/2-which-value-while/script.js
@@ -1,4 +1,7 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let i = 0;
+while (++i < 5) alert( i );
+// affiche 1 , 2 ,3, 4
+
+let i = 0;
+while (i++ < 5) alert( i );
+// affiche 1 ,  2, 3 , 4 , 5
